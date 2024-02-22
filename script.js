24da205d80d64e280a717ff20507ba974bcaf03a
lenpio0 <127386860+lenpio0@users.noreply.github.com>
2024-02-22 17:23:01 +0100

    added projects

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // Menu links
 let getAbout = document.getElementById("getAbout");
 let getResume = document.getElementById("getResume");
+let getProjects = document.getElementById("getProjects");
 let getContact = document.getElementById("getContact");
 
 // Sections
 let about = document.getElementById("about");
 let resume = document.getElementById("resume");
+let projects = document.getElementById("projects");
 let contact = document.getElementById("contact");
 
 function removeClass() {
     // Links
     getAbout.classList.remove('selected');
     getResume.classList.remove('selected');
+    getProjects.classList.remove('selected');
     getContact.classList.remove('selected');
     // Sections
     about.classList.remove('view');
     resume.classList.remove('view');
+    projects.classList.remove('view');
     contact.classList.remove('view');
 }
 
@@ -34,6 +38,14 @@ getResume.addEventListener('click', function (e) {
         removeClass();
         resume.classList.add('view');
         getResume.classList.add('selected');
+    }
+})
+getProjects.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        projects.classList.add('view');
+        getProjects.classList.add('selected');
     }
 })
 getContact.addEventListener('click', function (e) {
